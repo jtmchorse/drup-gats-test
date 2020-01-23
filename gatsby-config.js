7e00b07e6c08c612d06e1b887f93623a677e96fd
JT McHorse <jmchorse@gmail.com>
2020-01-23 09:27:20 -0800

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+plugins: [
+  {
+    resolve:'gatsby-source-drupal',
+    options: {
+      baseURL: "https://dev-smar-gatsby.pantheonsite.io/",
+      apiBase: 'jsonapi', // endpoint of Drupal server
+    },
+  },
+],
 }
